a0abf0a0fd25099a7bec4c3aba5e4e0d6f7bdad1
Jon Voelm <jonvoelm@gmail.com>
2024-09-10 06:34:27 -0600

    project files clean up given change to using Google Maps API

---
+++ Other Docs/retired code files/ASYNC_serversideimagescrape.js
@@ -0,0 +1,47 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs').promises;
+const path = require('path');
+
+////// CONFIGURATION //////
+const targetRoute = 'TEST_TEST' 
+//make the above same as "target_route" in config.py file
+
+const jsonFileName = targetRoute + '_coordinates.json'
+const zoom = 16; // set zoom level for Google Maps
+
+// Function to generate Google Maps URL
+function generateGoogleMapsUrl(lat, lng, zoom) {
+    return `https://www.google.com/maps/@${lat},${lng},${zoom}z/data=!5m1!1e1?authuser=0&entry=ttu`;
+}
+
+// Function to capture screenshot for a given coordinate
+async function captureScreenshot(browser, coord, zoom) {
+    const { lat, lng } = coord;
+    const page = await browser.newPage();
+    await page.setViewport({ width: 360, height: 800 });
+    const websiteUrl = generateGoogleMapsUrl(lat, lng, zoom);
+
+    await page.goto(websiteUrl, { waitUntil: 'networkidle0' });
+
+    const screenshotPath = `${targetRoute}_${lat}_${lng}_${zoom}x_360x800.png`;
+    await page.screenshot({ path: screenshotPath });
+
+    console.log(`Screenshot saved: ${screenshotPath}`);
+    await page.close();
+}
+
+// Main function to read coordinates and capture screenshots
+async function main() {
+    const coordinatesFilePath = path.join(__dirname, jsonFileName);
+    const coordinates = JSON.parse(await fs.readFile(coordinatesFilePath, 'utf8'));
+
+    const browser = await puppeteer.launch();
+
+    // Use Promise.all to handle multiple asynchronous operations concurrently
+    await Promise.all(coordinates.map(coord => captureScreenshot(browser, coord, zoom)));
+
+    console.log('All screenshots saved!');
+    await browser.close();
+}
+
+main().catch(console.error);
