65beba01a52edec794fb97215eb938aa6281f2a1
pyfection <mat@pyfection.com>
2024-10-07 17:20:33 -0300

    Add script to take source CSV files with express lane information and turn it into JSON

---
+++ scripts/us36_rates_to_json.py
@@ -0,0 +1,56 @@
+import csv
+import json
+
+toll_rates_file = "Other Docs/express lane rates/US36 express lane rates.csv"
+express_lanes_file = "src/resources/US36 EB express lane start and end points.csv"
+
+
+lanes = {}
+
+
+with open(toll_rates_file) as f:
+    reader = csv.reader(f)
+    # Skip two header rows
+    next(reader)
+    next(reader)
+    for row in reader:
+        if row[0] == "":
+            # Empty row reached, no more interesting information after that
+            break
+        elif row[0] == "Time of Day":
+            express_lane_names = row[1:]
+            for lane_name in express_lane_names:
+                lanes[lane_name] = {
+                    "lane_name": lane_name,
+                    "direction": "WB",
+                    "lines_start": {},
+                    "lines_end": {},
+                    "effective_start": {},
+                    "hours_tolls": {},
+                }
+        else:
+            tod, *express_lane_rates = row
+            for i, rate in enumerate(express_lane_rates):
+                lane_name = express_lane_names[i]
+                lane = lanes[lane_name]
+                lane["hours_tolls"][tod] = rate
+
+
+with open(express_lanes_file) as f:
+    reader = csv.reader(f)
+    next(reader)
+    for row in reader:
+        start_lat, start_lng, name, *_ = row
+        next(reader)
+        end_lat, end_lng, *_ = row
+        name = name.split()[0]
+        lane = lanes[name]
+        lane["lines_start"]["latitude"] = start_lat
+        lane["lines_start"]["longitude"] = start_lng
+        lane["lines_end"]["latitude"] = end_lat
+        lane["lines_end"]["longitude"] = end_lng
+
+
+result = list(lanes.values())
+
+print(json.dumps(result, indent=2))
