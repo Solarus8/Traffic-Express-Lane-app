51594264fae6fb94a7351e44a2d92ed63d600dd4
Jon Voelm <jonvoelm@gmail.com>
2024-11-01 21:08:17 -0600

    edited files and updated JSON generation code.  Generated JSONs for EB and WB US36 express lanes with all info except placeholder "0" values for distance (lines and effective)

--- us36_rates_to_json.py
+++ us36_rates_to_json.py
@@ -2,6 +2,7 @@ import csv
 import json
 
 toll_rates_file = "Other Docs/express lane rates/US36_rates/US36 EB express lane rates.csv"
+toll_liscence_rates_file = "Other Docs/express lane rates/US36_rates/US36 EB License express lane rates.csv"
 express_lanes_file = "src/resources/US36 EB express lane start and end points.csv"
 
 
@@ -31,6 +32,7 @@ with open(toll_rates_file) as f:
                     "lines_length": 0,
                     "effective_length": 0,
                     "hours_tolls": {},
+                    "hours_license_tolls": {}
                 }
         else:
             tod, *express_lane_rates = row
@@ -39,20 +41,42 @@ with open(toll_rates_file) as f:
                 lane = lanes[lane_name]
                 lane["hours_tolls"][tod] = rate
 
+with open(toll_liscence_rates_file) as f:
+    reader = csv.reader(f)
+    # Skip three header rows
+    next(reader)
+    next(reader)
+    next(reader)
+    for row in reader:
+        if row[0] == "":
+            # Empty row reached, no more interesting information after that
+            break
+        else:
+            tod, *express_lane_liscence_rates = row
+            for i, rate in enumerate(express_lane_liscence_rates):
+                lane_name = express_lane_names[i]
+                lane = lanes[lane_name]
+                lane["hours_license_tolls"][tod] = rate
 
 with open(express_lanes_file) as f:
     reader = csv.reader(f)
     next(reader)
     for row in reader:
-        start_lat, start_lng, name, *_ = row
-        next(reader)
-        end_lat, end_lng, *_ = row
-       # name = name.split()[0]
-        lane = lanes[name]
-        lane["lines_start"]["latitude"] = start_lat
-        lane["lines_start"]["longitude"] = start_lng
-        lane["lines_end"]["latitude"] = end_lat
-        lane["lines_end"]["longitude"] = end_lng
+        if row[0] == "":
+            # Empty row reached, no more interesting information after that
+            break
+        else: 
+            start_lat, start_lng,end_lat, end_lng, eff_start_lat, eff_start_lng, eff_end_lat, eff_end_lng, name, *_ = row
+            if name in lanes:
+                lane = lanes[name]
+                lane["lines_start"]["latitude"] = start_lat
+                lane["lines_start"]["longitude"] = start_lng
+                lane["lines_end"]["latitude"] = end_lat
+                lane["lines_end"]["longitude"] = end_lng
+                lane["effective_start"]["latitude"] = eff_start_lat
+                lane["effective_start"]["longitude"] = eff_start_lng
+                lane["effective_end"]["latitude"] = eff_end_lat
+                lane["effective_end"]["longitude"] = eff_end_lng
 
 result = list(lanes.values())
 
