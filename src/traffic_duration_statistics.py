8a1c84d80cb367fff21d5ef05e4a8dbf63ce808a
Jon Voelm <jonvoelm@gmail.com>
2024-11-02 19:28:35 -0600

    minor edits

--- src/traffic_duration_statistics.py
+++ src/traffic_duration_statistics.py
@@ -17,14 +17,15 @@ def print_duration_speed_traffic(data: dict):
     duration = leg["duration"]["value"]
     duration_in_traffic = leg["duration_in_traffic"]["value"]
     miles_hr = distance / duration_in_traffic * 3600 / 1609.34  # 1 mile = 1609.34 meters, 3600 seconds = 1 hour
-    print(datetime.now(), "\n", "Average duration: ", duration, "seconds.\n Live traffic duration: ", duration_in_traffic, "seconds.\n  ", round(miles_hr, 2), "miles/hr average")
+    print(datetime.now(), "\nDistance:", distance, "\nAverage duration: ", duration, "seconds.\n Live traffic duration: ", duration_in_traffic, "seconds.\n  ", round(miles_hr, 2), "miles/hr average")
 
 with open(EB_express_lanes) as f:
     express_lanes = json.load(f)
 with open(WB_express_lanes) as f:
     express_lanes.extend(json.load(f))
 
-results = []
+#AI code to get traffic data into JSON file
+""" results = []
 for lane in express_lanes:
     lane_data = {}
     lines_start = Coordinate(lane["lines_start"]["latitude"], lane["lines_start"]["longitude"])
@@ -51,19 +52,19 @@ for lane in express_lanes:
     results.append(lane_data)
 
 with open('traffic_data_results.json', 'w') as outfile:
-    json.dump(results, outfile, indent=4)
+    json.dump(results, outfile, indent=4) """
+#AI code to get traffic data into JSON file
 
 
 #below code prints to the console for testing
-""" for lane in express_lanes:
+for lane in express_lanes:
     lines_start = Coordinate(lane["lines_start"]["latitude"], lane["lines_start"]["longitude"])
     lines_end = Coordinate(lane["lines_end"]["latitude"], lane["lines_end"]["longitude"])
     effective_start = Coordinate(lane["effective_start"]["latitude"], lane["effective_start"]["longitude"])
     effective_end = Coordinate(lane["effective_end"]["latitude"], lane["effective_end"]["longitude"])
     data = get_traffic_from_api(effective_start, effective_end)
-    print(f'{lane["lane_name"]} {lane["road_name"]} {lane["direction"]} : effective_start to effective_end -->')
+    print(f'\n{lane["lane_name"]} {lane["road_name"]} {lane["direction"]} : effective_start to effective_end -->')
     print_duration_speed_traffic(data)
     data2 = get_traffic_from_api(lines_start, lines_end)
     print(f'{lane["lane_name"]} {lane["road_name"]} {lane["direction"]} : lines_start to lines_end -->')
     print_duration_speed_traffic(data2)
- """
